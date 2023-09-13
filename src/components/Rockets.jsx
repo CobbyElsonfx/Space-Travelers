e66e499a644b1bf06171460d15909f3b6bebc582
CarlosZ96 <c4r1oszambrano@gmail.com>
2023-09-13 15:56:03 -0500

    Feature:
    Add styles

--- src/components/Rockets.jsx
+++ src/components/Rockets.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getRockets } from '../redux/rockets/rocketSlice';
+import '../stylesheets/rockets.css';
 
 function Rockets() {
   const dispatch = useDispatch();
@@ -13,7 +14,9 @@ function Rockets() {
       {
         rockets.map((rocket) => (
           <div key={rocket.id} className="rocket-card">
-            <img src={rocket.flickr_images} alt="rocketimage" />
+            <div className="rocket-img-container">
+              <img src={rocket.flickr_images} alt="rocketimage" className="rocket-img" />
+            </div>
             <div className="text-container">
               <h2>{rocket.name}</h2>
               <p>{rocket.description}</p>
