f69136e58b741ebf73bb931586690d2c15913a9e
CarlosZ96 <c4r1oszambrano@gmail.com>
2023-09-13 14:30:32 -0500

    Feature:
    fetching the data from the API.

---
+++ src/components/view.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const view = () => (
+  <section className="rockets-container" />
+);
+
+export default view;
---
+++ src/redux/rockets/rocketSlice.jsx
@@ -0,0 +1,46 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+export const getRockets = createAsyncThunk(
+  'rockets/getRockets',
+  async (arg, { rejectWithValue }) => {
+    try {
+      const response = await axios.get(
+        'https://api.spacexdata.com/v3/rockets',
+      );
+      const rocketsApi = response.data;
+      const rocketsFilter = [];
+      rocketsApi.forEach((rocket) => {
+        const { id } = rocket;
+        const nameApi = rocket.rocket_name;
+        const TypeApi = rocket.rocket_type;
+        const imgApi = rocket.flickr_images[0];
+        rocketsFilter.push(
+          {
+            id,
+            name: nameApi,
+            type: TypeApi,
+            flickr_images: imgApi,
+          },
+        );
+      });
+      return rocketsFilter;
+    } catch (error) {
+      return rejectWithValue(error.response);
+    }
+  },
+);
+
+const rocketSlice = createSlice({
+  name: 'rockets',
+  initialState: {
+    rockets: [],
+  },
+  extraReducers: (builder) => {
+    builder.addCase(getRockets.fulfilled, (state, action) => {
+      state.rockets = action.payload;
+    });
+  },
+});
+
+export default rocketSlice.reducer;
