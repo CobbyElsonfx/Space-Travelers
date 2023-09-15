f5b2781743fd09db771654b627eed340aa6297c9
CarlosZ96 <c4r1oszambrano@gmail.com>
2023-09-15 18:51:13 -0500

    Feature:
    Styles for the rocket section finished.

--- src/components/MyRockets.jsx
+++ src/components/MyRockets.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import '../stylesheets/myrocketStyles.css';
 
 function MyRockets() {
   const rockets = useSelector((state) => state.rockets.rockets);
@@ -10,12 +11,12 @@ function MyRockets() {
       <tbody>
         {reservedRockets.length > 0 ? (
           reservedRockets.map((rocket) => (
-            <tr key={rocket.id}>
+            <tr key={rocket.id} className="name-profile">
               {rocket.name}
             </tr>
           ))
         ) : (
-          <h6>No Rockets reserved yet</h6>
+          <h3 className="name-profile">No Rockets reserved yet</h3>
         )}
       </tbody>
     </table>
